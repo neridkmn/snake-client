41596cc762877036b757a51f5cc3d9be05c6565c
Neriman Dikmen <sanerneriman@gmail.com>
2023-06-16 13:09:18 -0400

    Put connection inside the setupInput function. Updated handleUserInput with AWSD & CTRL+C commands. Lastly, exported the setupInput function.

--- input.js
+++ input.js
@@ -1,4 +1,8 @@
-const setupInput = function() {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -7,18 +11,24 @@ const setupInput = function() {
   stdin.on("data", handleUserInput);
 
   return stdin;
-};
-
-
-const handleUserInput = function() {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
 
 };
 
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === "w") {
+    connection.write("Move: up", () => console.log("Sent command: Move up"));
+  } else if (key === "a") {
+    connection.write("Move: left", () => console.log("Sent command: Move left"));
+  } else if (key === "s") {
+    connection.write("Move: down", () => console.log("Sent command: Move down"));
+  } else if (key === "d") {
+    connection.write("Move: right", () => console.log("Sent command: Move right"));
+  }
+};
 
 module.exports = {
   setupInput
